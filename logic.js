9b3d64703537e4059fb0fd2fc0c7f79d658dc907
Brigette Morando <bibimorando13@yahoo.com>
2016-03-23 15:47:04 -0700

    made spinner into colors and added css folder

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 
 
-
 $(".btn").click(function() {
   spinner();
 });
@@ -10,15 +9,26 @@ var number = Math.random()
 
 if(number <= 0.198){
   console.log("spot1")
+  console.log("red")
+  $(".red").addClass('changered')
 
 }else if(number <= 0.396){
   console.log("spot2")
+  console.log("blue")
+  $(".blue").addClass('changeblue')
 }else if(number <= 0.594){
   console.log("spot3")
+  console.log("green")
+  $(".green").addClass('changegreen')
 }else if(number <= 0.792){
   console.log("spot4")
+  console.log("purple")
+  $(".purple").addClass('changepurple')
 }else{
   console.log("spot5")
+  console.log("pink")
+  $(".pink").addClass('changepink')
 }
 console.log(number)
 };
+
